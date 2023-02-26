4a5f028fd43c8e57887f218110327531bc71d69d
Omprakash Patel <109479793+mroppatel@users.noreply.github.com>
2023-02-27 00:31:04 +0530

    Add files via upload

---
+++ p4.java
@@ -0,0 +1,8 @@
+class Sample {
+   public static void main(String[] args){
+     String s1 = Integer.toBinaryString(10);
+     String s2 = Integer.toBinaryString(-1);
+     System.out.println(s1);
+     System.out.println(s2); 
+   }
+}
